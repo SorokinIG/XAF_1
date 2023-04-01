6587d357cb43896660053c07362fe0ca0371ec4a
gimpl <gimpl@DESKTOP-A5T24MT>
2023-04-01 09:49:03 +0300

    Добавьте файлы проекта.

---
+++ XAF_1.Blazor.Server/BlazorApplication.cs
@@ -0,0 +1,47 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Xpo;
+using XAF_1.Blazor.Server.Services;
+
+namespace XAF_1.Blazor.Server;
+
+public class XAF_1BlazorApplication : BlazorApplication {
+    public XAF_1BlazorApplication() {
+        ApplicationName = "XAF_1";
+        CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+        DatabaseVersionMismatch += XAF_1BlazorApplication_DatabaseVersionMismatch;
+    }
+    protected override void OnSetupStarted() {
+        base.OnSetupStarted();
+#if DEBUG
+        if(System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+            DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+        }
+#endif
+    }
+    private void XAF_1BlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+        e.Updater.Update();
+        e.Handled = true;
+#else
+        if(System.Diagnostics.Debugger.IsAttached) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+        else {
+            string message = "The application cannot connect to the specified database, " +
+                "because the database doesn't exist, its version is older " +
+                "than that of the application or its schema does not match " +
+                "the ORM data model structure. To avoid this error, use one " +
+                "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+            if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+                message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+            }
+            throw new InvalidOperationException(message);
+        }
+#endif
+    }
+}
---
+++ XAF_1.Win/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System.Configuration;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Win.ApplicationBuilder;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Win;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.XtraEditors;
+using DevExpress.ExpressApp.Design;
+
+namespace XAF_1.Win;
+
+public class ApplicationBuilder : IDesignTimeApplicationFactory {
+    public static WinApplication BuildApplication(string connectionString) {
+        var builder = WinApplication.CreateBuilder();
+        builder.UseApplication<XAF_1WindowsFormsApplication>();
+        builder.Modules
+            .AddAuditTrailXpo()
+            .AddConditionalAppearance()
+            .AddReports(options => {
+                options.EnableInplaceReports = true;
+                options.ReportDataType = typeof(DevExpress.Persistent.BaseImpl.ReportDataV2);
+                options.ReportStoreMode = DevExpress.ExpressApp.ReportsV2.ReportStoreModes.XML;
+            })
+            .AddTreeListEditors()
+            .AddValidation(options => {
+                options.AllowValidationDetailsAccess = false;
+            })
+            .AddViewVariants()
+            .Add<XAF_1.Module.XAF_1Module>()
+        	.Add<XAF_1WinModule>();
+        builder.ObjectSpaceProviders
+            .AddXpo((application, options) => {
+                options.ConnectionString = connectionString;
+            })
+            .AddNonPersistent();
+        builder.AddBuildStep(application => {
+            application.ConnectionString = connectionString;
+#if DEBUG
+            if(System.Diagnostics.Debugger.IsAttached && application.CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+                application.DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+            }
+#endif
+        });
+        var winApplication = builder.Build();
+        return winApplication;
+    }
+
+    XafApplication IDesignTimeApplicationFactory.Create()
+        => BuildApplication(XafApplication.DesignTimeConnectionString);
+}
